f8034f48b1c9e4873ba8dd1314d944376dae17ca
awkward-3312 <betza_es77@hotmail.com>
2026-08-17 19:26:12 -0600

    Add Spanish password reset messages

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'reset' => 'Tu contraseña ha sido restablecida.',
+    'sent' => 'Te enviamos por correo el enlace para restablecer tu contraseña.',
+    'throttled' => 'Espera un momento antes de volver a intentarlo.',
+    'token' => 'Este enlace para restablecer la contraseña no es válido o ha vencido.',
+    'user' => 'No encontramos un usuario con ese correo electrónico.',
+];
