1f8a07d99ee5d806dcdb5b8c74b2536b467b5f7a
awkward-3312 <betza_es77@hotmail.com>
2026-08-18 22:01:32 -0600

    Translate login validation messages to Spanish

--- resources/src/login.js
+++ resources/src/login.js
@@ -31,18 +31,19 @@ Vue.use(Meta, {
 });
 
 localize({
-  en: {
+  es: {
     messages: {
-      required: 'This field is required',
-      required_if: 'This field is required',
-      regex: 'This field must be a valid',
-      mimes: `This field must have a valid file type.`,
-      size: (_, { size }) => `This field size must be less than ${size}.`,
-      min: 'This field must have no less than {length} characters',
-      max: (_, { length }) => `This field must have no more than ${length} characters`
+      required: 'Este campo es obligatorio',
+      required_if: 'Este campo es obligatorio',
+      regex: 'Este campo debe tener un formato válido',
+      mimes: 'Este archivo debe tener un tipo válido',
+      size: (_, { size }) => `El tamaño del archivo debe ser menor de ${size}`,
+      min: 'Este campo debe tener al menos {length} caracteres',
+      max: (_, { length }) => `Este campo no puede tener más de ${length} caracteres`
     }
   },
 });
+localize('es');
 // Install VeeValidate rules and localization
 Object.keys(rules).forEach(rule => {
   extend(rule, rules[rule]);
