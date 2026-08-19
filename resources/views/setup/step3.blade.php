522f54ce832bcf11fdeb8827264c6bacdc03a487
awkward-3312 <betza_es77@hotmail.com>
2026-08-18 23:09:50 -0600

    Translate installer summary step to Spanish

--- resources/views/setup/step3.blade.php
+++ resources/views/setup/step3.blade.php
@@ -6,33 +6,33 @@
     <div class="row">
         <div class="col-12 text-center mt-3">
             <ul class="progressbar">
-                <li class="active">Server Requirements</li>
-                <li class="active"><a href="/setup">Settings</a></li>
-                <li class="active"><a href="/setup/step-2">Database</a></li>
-                <li class="active"><a href="/setup/step-3">Summary</a></li>
+                <li class="active">Requisitos del servidor</li>
+                <li class="active"><a href="/setup">Configuración</a></li>
+                <li class="active"><a href="/setup/step-2">Base de datos</a></li>
+                <li class="active"><a href="/setup/step-3">Resumen</a></li>
             </ul>
         </div>
     </div>
 
     <div class="row mt-3">
-        <div class="loader d-none">Loading...</div>
+        <div class="loader d-none">Cargando...</div>
     </div>
 
     <div class="row mt-3 p-5 d-block" id="content">
 
         <div class="col-12">
 
-            <form  action="{{route('lastStep')}}" method="post">
+            <form action="{{route('lastStep')}}" method="post">
                 @csrf
 
-                <h2 class="mb-5">Do you want these settings to change?</h2>
+                <h2 class="mb-5">¿Deseas aplicar esta configuración?</h2>
 
                 <div id="tochange">
 
                 @if($data['APP_NAME'] != 'old')
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-12 col-md-6 text-truncate">Application Name</div>
+                            <div class="col-12 col-md-6 text-truncate">Nombre de la aplicación</div>
 
                             <div class="col-12 col-md-6 text-truncate"> {{ $data['APP_NAME'] }}</div>
                         </div>
@@ -42,7 +42,7 @@
                 @if($data['APP_KEY'] != 'old')
                 <div class="form-group">
                     <div class="row">
-                        <div class="col-12 col-md-6 text-truncate font-weight-bold">Application Key</div>
+                        <div class="col-12 col-md-6 text-truncate font-weight-bold">Clave de la aplicación</div>
 
                         <div class="col-12 col-md-6 text-truncate"> {{ $data['APP_KEY'] }}</div>
                     </div>
@@ -52,7 +52,7 @@
                 @if($data['APP_DEBUG'] != 'old')
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-12 col-md-6 text-truncate ">Application Debug Mode</div>
+                            <div class="col-12 col-md-6 text-truncate">Modo de depuración</div>
 
                             <div class="col-12 col-md-6 text-truncate"> {{ $data['APP_DEBUG'] }}</div>
                         </div>
@@ -63,7 +63,7 @@
                 @if($data['DB_HOST'] != 'old')
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-12 col-md-6 text-truncate">Database Host</div>
+                            <div class="col-12 col-md-6 text-truncate">Servidor de base de datos</div>
 
                             <div class="col-12 col-md-6 text-truncate"> {{ $data['DB_HOST'] }}</div>
                         </div>
@@ -74,7 +74,7 @@
                 @if($data['DB_DATABASE'] != 'old')
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-12 col-md-6 text-truncate">Database Selected</div>
+                            <div class="col-12 col-md-6 text-truncate">Base de datos seleccionada</div>
 
                             <div class="col-12 col-md-6 text-truncate"> {{ $data['DB_DATABASE']}}</div>
                         </div>
@@ -84,7 +84,7 @@
                 @if($data['DB_USERNAME'] != 'old')
                     <div class="form-group">
                         <div class="row">
-                            <div class="col-12 col-md-6 text-truncate">Database Username</div>
+                            <div class="col-12 col-md-6 text-truncate">Usuario de la base de datos</div>
 
                             <div class="col-12 col-md-6 text-truncate"> {{ $data['DB_USERNAME'] }}</div>
                         </div>
@@ -95,10 +95,10 @@
                 </div>
                 <div class="row mt-5">
                     <div class="col-12 col-md-6 text-truncate">
-                        <a href="/setup/step-2" class="btn btn-outline-danger mb-2"  ><i class="fa fa-angle-left"></i> Previous Step </a>
+                        <a href="/setup/step-2" class="btn btn-outline-danger mb-2"><i class="fa fa-angle-left"></i> Paso anterior</a>
                     </div>
                     <div class="col-12 col-md-6 text-truncate">
-                        <button  type="submit" class="btn btn-success mb-2 btn-block" id="lastStep"  >Confirm <i class="fa fa-check"></i></button>
+                        <button type="submit" class="btn btn-success mb-2 btn-block" id="lastStep">Confirmar <i class="fa fa-check"></i></button>
                     </div>
                 </div>
             </form>
