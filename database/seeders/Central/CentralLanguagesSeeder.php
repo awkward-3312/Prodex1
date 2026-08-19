b03524a4fd6ef50fbbd71101f57ae2c9a194a34e
awkward-3312 <betza_es77@hotmail.com>
2026-08-18 22:08:16 -0600

    Make Spanish the central default language

--- database/seeders/Central/CentralLanguagesSeeder.php
+++ database/seeders/Central/CentralLanguagesSeeder.php
@@ -10,15 +10,15 @@ class CentralLanguagesSeeder extends Seeder
     public function run(): void
     {
         $languages = [
-            ['name' => 'English',    'locale' => 'en', 'flag' => 'gb.svg', 'is_default' => true,  'is_rtl' => false, 'sort_order' => 1],
-            ['name' => 'French',     'locale' => 'fr', 'flag' => 'fr.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 2],
-            ['name' => 'Arabic',     'locale' => 'ar', 'flag' => 'sa.svg', 'is_default' => false, 'is_rtl' => true,  'sort_order' => 3],
-            ['name' => 'Spanish',    'locale' => 'es', 'flag' => 'es.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 4],
+            ['name' => 'Español',    'locale' => 'es', 'flag' => 'es.svg', 'is_default' => true,  'is_rtl' => false, 'sort_order' => 1],
+            ['name' => 'Inglés',     'locale' => 'en', 'flag' => 'gb.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 2],
+            ['name' => 'Francés',    'locale' => 'fr', 'flag' => 'fr.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 3],
+            ['name' => 'Árabe',      'locale' => 'ar', 'flag' => 'sa.svg', 'is_default' => false, 'is_rtl' => true,  'sort_order' => 4],
             ['name' => 'Hindi',      'locale' => 'hi', 'flag' => 'in.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 5],
-            ['name' => 'Bengali',    'locale' => 'bn', 'flag' => 'bd.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 6],
-            ['name' => 'Turkish',    'locale' => 'tr', 'flag' => 'tr.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 7],
-            ['name' => 'German',     'locale' => 'de', 'flag' => 'de.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 8],
-            ['name' => 'Portuguese', 'locale' => 'pt', 'flag' => 'pt.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 9],
+            ['name' => 'Bengalí',    'locale' => 'bn', 'flag' => 'bd.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 6],
+            ['name' => 'Turco',      'locale' => 'tr', 'flag' => 'tr.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 7],
+            ['name' => 'Alemán',     'locale' => 'de', 'flag' => 'de.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 8],
+            ['name' => 'Portugués',  'locale' => 'pt', 'flag' => 'pt.svg', 'is_default' => false, 'is_rtl' => false, 'sort_order' => 9],
             ['name' => 'Urdu',       'locale' => 'ur', 'flag' => 'pk.svg', 'is_default' => false, 'is_rtl' => true,  'sort_order' => 10],
         ];
 
