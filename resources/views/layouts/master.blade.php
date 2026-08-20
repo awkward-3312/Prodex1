d21b6460571761e7d36687495eca67ffc1493cac
awkward-3312 <betza_es77@hotmail.com>
2026-08-20 15:29:05 -0600

    Load PRODEX navigation v3 globally

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -55,6 +55,7 @@
 
     <script src="/js/main.min.js?v=1.3&v={{ time() }}"></script>
     <script src="/js/prodex-sidebar2-organizer.js?v={{ time() }}"></script>
+    <script src="/js/prodex-navigation-v3.js?v={{ time() }}"></script>
 
     @include('partials.plan-upgrade-modal')
     @include('partials.subscription-reminder-banner')
