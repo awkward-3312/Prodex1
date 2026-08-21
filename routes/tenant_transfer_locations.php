5bd85eb2a9df963b316e5b97fbdf83e7ced0db0a
awkward-3312 <betza_es77@hotmail.com>
2026-08-21 17:57:18 -0600

    Route inventory-location transfer APIs

---
+++ routes/tenant_transfer_locations.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', 'tenant.subscribed', 'tenant.activity'])
+    ->prefix('transfer-location')
+    ->group(function () {
+        Route::get('/options', [\App\Http\Controllers\TransferLocationController::class, 'options']);
+        Route::get('/{locationId}/products', [\App\Http\Controllers\TransferLocationController::class, 'products'])->whereNumber('locationId');
+        Route::get('/{locationId}/products/{productId}', [\App\Http\Controllers\TransferLocationController::class, 'product'])
+            ->whereNumber('locationId')->whereNumber('productId');
+        Route::get('/{locationId}/batches/{productId}/{variantId?}', [\App\Http\Controllers\TransferLocationController::class, 'batches'])
+            ->whereNumber('locationId')->whereNumber('productId')->whereNumber('variantId');
+    });
