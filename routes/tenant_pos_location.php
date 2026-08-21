dfe52bd69ea6860cb36cc9da86de9762f9286a71
awkward-3312 <betza_es77@hotmail.com>
2026-08-21 17:04:41 -0600

    Expose bulk POS inventory location stock APIs

--- routes/tenant_pos_location.php
+++ routes/tenant_pos_location.php
@@ -4,6 +4,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', 'tenant.subscribed', 'tenant.activity'])
     ->group(function () {
+        Route::get('/pos/location-inventory/{locationId}/stock-map', [
+            \App\Http\Controllers\PosLocationInventoryController::class,
+            'stockMap',
+        ])->whereNumber('locationId');
+
+        Route::get('/pos/location-inventory/{locationId}/changes', [
+            \App\Http\Controllers\PosLocationInventoryController::class,
+            'changes',
+        ])->whereNumber('locationId');
+
         Route::get('/pos/location-inventory/{locationId}/products/{productId}', [
             \App\Http\Controllers\PosLocationInventoryController::class,
             'show',
