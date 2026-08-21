65d5a92bf72a31faf541ffc1edd290201a202f42
awkward-3312 <betza_es77@hotmail.com>
2026-08-21 15:38:41 -0600

    Protect organization user creation with plan limits

--- routes/tenant_organization.php
+++ routes/tenant_organization.php
@@ -11,8 +11,13 @@ Route::middleware('auth:api')->prefix('organization')->group(function () {
     Route::put('branches/{branchId}/employees/{employeeId}', 'Organization\\BranchController@assignEmployee');
     Route::post('branches/{branchId}/inventory-locations', 'Organization\\BranchController@storeInventoryLocation');
 
+    // User management exists independently from HRM. Creating a login consumes
+    // the same max_users plan limit regardless of whether it starts from an employee.
+    Route::get('user-access/options', 'Organization\\UserAccessController@options');
+    Route::post('user-access', 'Organization\\UserAccessController@store')->middleware('tenant.limit:max_users');
+
     Route::get('employee-access', 'Organization\\EmployeeAccessController@index');
-    Route::post('employee-access/{employeeId}/create', 'Organization\\EmployeeAccessController@create');
+    Route::post('employee-access/{employeeId}/create', 'Organization\\EmployeeAccessController@create')->middleware('tenant.limit:max_users');
     Route::put('employee-access/{employeeId}/link', 'Organization\\EmployeeAccessController@link');
     Route::delete('employee-access/{employeeId}/link', 'Organization\\EmployeeAccessController@unlink');
 
