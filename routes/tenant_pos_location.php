c755688481279095d5261beab71e6b65b7ef1005
awkward-3312 <betza_es77@hotmail.com>
2026-08-21 17:12:54 -0600

    Expose native POS catalog for inventory locations

--- routes/tenant_pos_location.php
+++ routes/tenant_pos_location.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', 'tenant.subscribed', 'tenant.activity'])
     ->group(function () {
+        Route::get('/pos/location-inventory/{locationId}/catalog', [
+            \App\Http\Controllers\PosLocationCatalogController::class,
+            'index',
+        ])->whereNumber('locationId');
+
         Route::get('/pos/location-inventory/{locationId}/stock-map', [
             \App\Http\Controllers\PosLocationInventoryController::class,
             'stockMap',
