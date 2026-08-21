144f06568171feb08eb3e060a80d8105be4f1911
awkward-3312 <betza_es77@hotmail.com>
2026-08-21 17:58:35 -0600

    Expose transfer location context

--- routes/tenant_transfer_locations.php
+++ routes/tenant_transfer_locations.php
@@ -6,6 +6,7 @@ Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', '
     ->prefix('transfer-location')
     ->group(function () {
         Route::get('/options', [\App\Http\Controllers\TransferLocationController::class, 'options']);
+        Route::get('/transfers/{transferId}/context', [\App\Http\Controllers\TransferLocationController::class, 'context'])->whereNumber('transferId');
         Route::get('/{locationId}/products', [\App\Http\Controllers\TransferLocationController::class, 'products'])->whereNumber('locationId');
         Route::get('/{locationId}/products/{productId}', [\App\Http\Controllers\TransferLocationController::class, 'product'])
             ->whereNumber('locationId')->whereNumber('productId');
