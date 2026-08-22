e3377934f88b1e6975f92183c4230a412fe449e1
awkward-3312 <betza_es77@hotmail.com>
2026-08-21 19:22:31 -0600

    Expose location-aware transfer receiving payloads

--- routes/tenant_transfer_logistics.php
+++ routes/tenant_transfer_logistics.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', 'tenant.subscribed', 'tenant.activity'])->group(function () {
-    Route::get('/transfer-logistics/incoming', 'TransferLogisticsController@incoming');
-    Route::get('/transfer-logistics/notifications', 'TransferLogisticsController@notifications');
-    Route::post('/transfer-logistics/notifications/{notificationId}/read', 'TransferLogisticsController@markNotificationRead');
+    Route::get('/transfer-logistics/incoming', 'FinalTransferLogisticsController@incoming');
+    Route::get('/transfer-logistics/notifications', 'FinalTransferLogisticsController@notifications');
+    Route::post('/transfer-logistics/notifications/{notificationId}/read', 'FinalTransferLogisticsController@markNotificationRead');
 
     Route::get('/transfer-logistics/issues', 'TransferDiscrepancyController@index');
     Route::post('/transfer-logistics/issues/{id}/resolve', 'LocationAwareTransferDiscrepancyController@resolve');
 
-    Route::get('/transfer-logistics/scan/{token}', 'TransferLogisticsController@showByToken');
-    Route::get('/transfer-logistics/{id}/qr', 'TransferLogisticsController@qrPayload');
-    Route::get('/transfer-logistics/{id}', 'TransferLogisticsController@show');
-    Route::post('/transfer-logistics/{id}/receive', 'TransferLogisticsController@receive');
+    Route::get('/transfer-logistics/scan/{token}', 'FinalTransferLogisticsController@showByToken');
+    Route::get('/transfer-logistics/{id}/qr', 'FinalTransferLogisticsController@qrPayload');
+    Route::get('/transfer-logistics/{id}', 'FinalTransferLogisticsController@show');
+    Route::post('/transfer-logistics/{id}/receive', 'FinalTransferLogisticsController@receive');
 });
