dadc38f0d13eabb4d92b7deabbab0e7cb4f6632e
awkward-3312 <betza_es77@hotmail.com>
2026-08-24 15:57:40 -0600

    Load inventory SPA navigation helper

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -62,6 +62,7 @@
     <script src="/js/prodex-transfer-issues.js?v={{ time() }}"></script>
     <script src="/js/prodex-damage-location-ui.js?v={{ time() }}"></script>
     <script src="/js/prodex-inventory-visibility.js?v={{ time() }}"></script>
+    <script src="/js/prodex-inventory-spa-navigation.js?v={{ time() }}"></script>
     <script src="/js/prodex-pos-location-ui.js?v={{ time() }}"></script>
     <script src="/js/prodex-pos-location-catalog.js?v={{ time() }}"></script>
     <script src="/js/prodex-pos-location-offline.js?v={{ time() }}"></script>
