b214f366e567e5efd6afad6ae0463a1af71693c2
awkward-3312 <betza_es77@hotmail.com>
2026-08-25 20:00:36 -0600

    Route cash register report through safe controller

--- routes/tenant_pos_reports.php
+++ routes/tenant_pos_reports.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', 'tenant.subscribed', 'tenant.activity'])
     ->group(function () {
-        Route::get('report/cash_registers_native', 'PosCashRegisterReportController@report');
+        Route::get('report/cash_registers_native', 'SafePosCashRegisterReportController@report');
     });
