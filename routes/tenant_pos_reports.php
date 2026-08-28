02b7aaf00cdf4311b5c852ecf36b44d01deb5e83
awkward-3312 <betza_es77@hotmail.com>
2026-08-27 20:28:57 -0600

    Use time-aware operational sales reports

--- routes/tenant_pos_reports.php
+++ routes/tenant_pos_reports.php
@@ -12,10 +12,10 @@ Route::middleware(['auth:api', 'Is_Active', 'request.safety', 'token.timeout', '
         Route::get('dashboard_data', 'OperationalDashboardController@dashboard_data');
         Route::get('sales', 'OperationalSalesController@index');
 
-        Route::get('report/sales', 'OperationalReportController@Report_Sales');
+        Route::get('report/sales', 'TimeAwareOperationalReportController@Report_Sales');
         Route::get('report/users', 'OperationalReportController@users_Report');
         Route::get('report/get_sales_by_user', 'OperationalReportController@get_sales_by_user');
-        Route::get('report/seller_report', 'OperationalReportController@seller_report');
+        Route::get('report/seller_report', 'TimeAwareOperationalReportController@seller_report');
         Route::get('report/top_products', 'OperationalReportController@report_top_products');
         Route::get('report/top_customers', 'OperationalReportController@report_top_customers');
         Route::get('report/sales_by_category_report', 'OperationalReportController@sales_by_category_report');
